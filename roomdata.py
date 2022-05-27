828abe89d25296e35e93364efd174fb966a7c8f2
kriskraw <chriscrawley80@gmail.com>
2022-05-27 19:03:04 +0000

    303

---
+++ roomdata.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+## A dictionary linking a room to other rooms
+rooms = {
+            'Hall' : {
+                  'items'  : ['key','letter','dagger'],
+                  'directions'  : {'south' : 'Kitchen','east'  : 'Dining Room','north'  : 'Library','west'  : 'Stairs'}
+                },
+            'Kitchen' : {
+                  'items'  : ['Broccoli'],
+                  'directions'  : {'north' : 'Hall'}
+                },
+            'Dining Room' : {
+                  'items'  : ['candle stick','bottle of vodka','potion'],
+                  'directions'  : {'west' : 'Hall','south': 'Garden','north' : 'Pantry'}
+               },
+            'Garden' : {
+                  'items'  : ['ax','shovel'],
+                  'directions'  : {'north' : 'Dining Room'},
+               },
+            'Pantry' : {
+                  'items' : ['cookie', 'rat poison'],
+                  'directions'  : {'south' : 'Dining Room'}
+            },
+            'Library' : {
+                  'items' : ['letter opener', 'bible'],
+                  'directions'  : {'west' : 'Garden'}
+            },
+            'Stairs' : {
+                  'items' : ['dagger'],
+                  'directions'  : {'north' : {'Kitchen'}}
+            }
+         }
--- rpg.py
+++ rpg.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from roomdata import rooms
 import random
 
 
@@ -42,39 +43,6 @@ def showStatus():
 
 #an inventory, which is initially empty
 inventory = []
-
-#a dictionary linking a room to other rooms
-## A dictionary linking a room to other rooms
-rooms = {
-            'Hall' : {
-                  'items'  : ['key','letter','dagger'],
-                  'directions'  : {'south' : 'Kitchen','east'  : 'Dining Room','north'  : 'Library','west'  : 'Stairs'}
-                },
-            'Kitchen' : {
-                  'items'  : ['Broccoli'],
-                  'directions'  : {'north' : 'Hall'}
-                },
-            'Dining Room' : {
-                  'items'  : ['candle stick','bottle of vodka','potion'],
-                  'directions'  : {'west' : 'Hall','south': 'Garden','north' : 'Pantry'}
-               },
-            'Garden' : {
-                  'items'  : ['ax','shovel'],
-                  'directions'  : {'north' : 'Dining Room'},
-               },
-            'Pantry' : {
-                  'items' : ['cookie', 'rat poison'],
-                  'directions'  : {'south' : 'Dining Room'}
-            },
-            'Library' : {
-                  'items' : ['letter opener', 'bible'],
-                  'directions'  : {'west' : 'Garden'}
-            },
-            'Stairs' : {
-                  'items' : ['dagger'],
-                  'directions'  : {'north' : {'Kitchen'}}
-            }
-         }
 
 #start the player in the Hall
 currentRoom = 'Hall'
