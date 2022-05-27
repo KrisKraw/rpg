eed1a0454d2e34e0db800339799647a894923613
kriskraw <chriscrawley80@gmail.com>
2022-05-27 18:39:41 +0000

    239

--- rpg.py
+++ rpg.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
+import random
+
 
 def showInstructions():
   #print a main menu and the commands
   print('''
 RPG Game
 ========
+
+to win, pickup a dagger, ax or rat posion to kill the monster or 
+go into the Garden with a key and potion.
+The monster can be in any room.
+
 Commands:
   go [direction]
   get [item]
@@ -44,7 +51,7 @@ rooms = {
                   'directions'  : {'south' : 'Kitchen','east'  : 'Dining Room','north'  : 'Library','west'  : 'Stairs'}
                 },
             'Kitchen' : {
-                  'items'  : ['monster'],
+                  'items'  : ['Broccoli'],
                   'directions'  : {'north' : 'Hall'}
                 },
             'Dining Room' : {
@@ -73,6 +80,13 @@ rooms = {
 currentRoom = 'Hall'
 
 showInstructions()
+
+#randomizes monster's location
+roomList = list(rooms)
+monstersLocation = random.randint(0,len(rooms)-1)
+monstersLocation = roomList[monstersLocation]
+monstersRoom = rooms.get(monstersLocation)
+monstersRoom['items'].append('monster')
 
 #loop forever
 while True:
@@ -117,12 +131,14 @@ while True:
       #tell them they can't get it
       print('Can\'t get ' + move[1] + '!')
       
-  ## Define how a player can win
+  ## Defines how a player can win
+  ## If a player enters the Garden and has a key and potion, they WIN
   if currentRoom == 'Garden' and 'key' in inventory and 'potion' in inventory:
     print('You escaped the house with the ultra rare key and magic potion... YOU WIN!')
     break
 
-  ## If a player enters a room with a monster
+  ## If a player enters a room with the monster and has a dagger, ax, or rat poison, they WIN
+  ## else they LOOSE
   elif 'items' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['items']:
     if 'dagger' in inventory or 'ax' in inventory or 'rat poison' in inventory:
         print('You killed the monster... YOU WIN!')
